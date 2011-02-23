d78e94520d1c59d0d0f6dbcc536974d2fbad8d34
fthiery <fthiery@145cf96b-b085-dcf2-7ec3-c09ff84cf5e9>
2011-02-23 17:23:50 +0000

    fix auth messages viewing

--- trunk/quickshell/plugins/default.py
+++ trunk/quickshell/plugins/default.py
@@ -4,7 +4,9 @@
 
 import socket
 
+log_viewing_template = 'sudo less +F %s'
 default_network = 'eth0'
+
 commands = (
     {'title': 'System Monitoring', 'category': True},
     {'title': 'cpu & mem usage (htop)', 'cmd': 'htop', 'key': 'h'},
@@ -12,6 +14,6 @@ commands = (
     {'title': 'network usage (nettop)', 'cmd': 'sudo jnettop -i %s' %default_network, 'key': 'n'},
     {'title': 'network sniffing (tcpdump)', 'cmd': 'sudo tcpdump not host %s -i %s -vvv -A' %(socket.gethostname(),default_network)},
     {'title': 'Log viewing', 'category': True},
-    {'title': '/var/log/messages', 'cmd': 'sudo tail -f /var/log/messages', 'key': 'm'},
-    {'title': '/var/log/auth', 'cmd': 'sudo tail -f /var/log/auth', 'key': 'a'},
+    {'title': '/var/log/messages', 'cmd': log_viewing_template %'/var/log/messages' , 'key': 'm'},
+    {'title': '/var/log/auth', 'cmd': log_viewing_template %'/var/log/auth.log', 'key': 'a'},
 )
