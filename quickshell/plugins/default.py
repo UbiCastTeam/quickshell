abffff45bc90fcac3e76e40e6823b76b9889a952
fthiery <fthiery@145cf96b-b085-dcf2-7ec3-c09ff84cf5e9>
2011-02-23 18:18:40 +0000

    added df -h

--- quickshell/plugins/default.py
+++ quickshell/plugins/default.py
@@ -11,6 +11,7 @@ commands = (
     {'title': 'System Monitoring', 'category': True},
     {'title': 'cpu & mem usage (htop)', 'cmd': 'htop', 'key': 'h'},
     {'title': 'disk activity usage (iotop)', 'cmd': 'iotop', 'key': 'i'},
+    {'title': 'disk space', 'cmd': 'watch "df -h"'},
     {'title': 'network usage (nettop)', 'cmd': 'sudo jnettop -i %s' %default_network, 'key': 'n'},
     {'title': 'network sniffing (tcpdump)', 'cmd': 'sudo tcpdump not host %s -i %s -vvv -A' %(socket.gethostname(),default_network)},
     {'title': 'Log viewing', 'category': True},
